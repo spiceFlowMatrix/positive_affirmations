7aa3d0f59ffaa236573c28b3c138f12b6dcc04f3
HRahimy <rahimy.hamza@gmail.com>
2021-05-04 05:28:44 +0300

    Add tests ensure correct form validation UI

--- src/mobile_app/test/account_setup/widgets/name_form_screen_test.dart
+++ src/mobile_app/test/account_setup/widgets/name_form_screen_test.dart
@@ -3,10 +3,13 @@ import 'dart:math';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:formz/formz.dart';
 import 'package:mobile_app/account_setup/blocs/sign_up/sign_up_bloc.dart';
+import 'package:mobile_app/account_setup/models/models.dart';
 import 'package:mobile_app/account_setup/widgets/name_form_screen.dart';
 import 'package:mobile_app/positive_affirmations_keys.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:formz/formz.dart';
 
 class FakeSignUpEvent extends Fake implements SignUpEvent {}
 
@@ -14,6 +17,8 @@ class FakeSignUpState extends Fake implements SignUpState {}
 
 class MockSignUpBloc extends MockBloc<SignUpEvent, SignUpState>
     implements SignUpBloc {}
+
+class MockNameField extends Mock implements NameField {}
 
 void main() {
   group('NameFormScreen', () {
@@ -36,6 +41,60 @@ void main() {
           find.byKey(PositiveAffirmationsKeys.nameFieldLabel), findsOneWidget);
       expect(find.byKey(PositiveAffirmationsKeys.nameSubmitButton),
           findsOneWidget);
+    });
+
+    testWidgets('Empty form cannot be submitted', (tester) async {
+      when(() => signUpBloc.state)
+          .thenReturn(const SignUpState(nameStatus: FormzStatus.pure));
+
+      await tester.pumpWidget(MaterialApp(home: NameFormScreen()));
+
+      expect(
+        tester
+            .widget<ElevatedButton>(
+                find.byKey(PositiveAffirmationsKeys.nameSubmitButton))
+            .enabled,
+        isFalse,
+      );
+    });
+
+    testWidgets('Error text is rendered when validation errors exist',
+        (tester) async {
+      final nameField = MockNameField();
+      when(() => nameField.error).thenReturn(NameFieldValidationError.invalid);
+      when(() => signUpBloc.state).thenReturn(
+        SignUpState(
+          name: nameField,
+          nameStatus: FormzStatus.invalid,
+        ),
+      );
+
+      await tester.pumpWidget(MaterialApp(home: NameFormScreen()));
+
+      expect(
+        tester
+            .widget<TextField>(find.byKey(PositiveAffirmationsKeys.nameField))
+            .decoration!
+            .errorText,
+        isNotNull,
+      );
+    });
+
+    testWidgets('Error text only shows when name field is not pure',
+        (tester) async {
+      final nameField = MockNameField();
+      when(() => nameField.pure).thenReturn(true);
+      when(() => signUpBloc.state).thenReturn(SignUpState(name: nameField));
+
+      await tester.pumpWidget(MaterialApp(home: NameFormScreen()));
+
+      expect(
+        tester
+            .widget<TextField>(find.byKey(PositiveAffirmationsKeys.nameField))
+            .decoration!
+            .errorText,
+        isNull,
+      );
     });
   });
 }
