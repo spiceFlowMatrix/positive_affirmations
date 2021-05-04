472da02fe9b784e5f8b54c5235fcfdda73e66405
HRahimy <rahimy.hamza@gmail.com>
2021-05-04 03:26:26 +0300

    Add NameFormScreen components with keys

--- src/mobile_app/lib/account_setup/widgets/name_form_screen.dart
+++ src/mobile_app/lib/account_setup/widgets/name_form_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/positive_affirmations_keys.dart';
+import 'package:mobile_app/positive_affirmations_theme.dart';
 
 class NameFormScreen extends StatelessWidget {
   static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -7,11 +9,39 @@ class NameFormScreen extends StatelessWidget {
     return MaterialPageRoute<void>(builder: (_) => NameFormScreen());
   }
 
-  // TODO: Write tests to check that keys are supplied
-  // TODO: Add keys to components
+  Widget _buildLabel() => RichText(
+        key: PositiveAffirmationsKeys.nameFieldLabel,
+        text: TextSpan(
+          style: PositiveAffirmationsTheme.theme.textTheme.headline1?.copyWith(
+            fontSize: 23,
+            fontWeight: FontWeight.bold,
+          ),
+          children: [
+            TextSpan(
+                text: 'Hi. My name\'s Buddy\n',
+                style: TextStyle(color: Colors.grey)),
+            const TextSpan(text: 'What\'s your name?'),
+          ],
+        ),
+      );
+
+  Widget _buildNameField() => const TextField(
+        key: PositiveAffirmationsKeys.nameField,
+        decoration: InputDecoration(
+          labelText: 'Name',
+        ),
+      );
+
+  Widget _buildSubmitButton() => ElevatedButton(
+        key: PositiveAffirmationsKeys.nameSubmitButton,
+        onPressed: () {},
+        child: Text('NEXT'),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: PositiveAffirmationsKeys.nameFormScreen,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 35),
         child: Form(
@@ -21,27 +51,11 @@ class NameFormScreen extends StatelessWidget {
             child: ListView(
               shrinkWrap: true,
               children: [
-                const Text(
-                  'Hi. My name\'s Buddy',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                    fontSize: 23,
-                  ),
-                ),
-                const Text(
-                  'What\'s your name?',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 23,
-                  ),
-                ),
+                _buildLabel(),
                 const Padding(padding: EdgeInsets.only(top: 10)),
-                const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                  ),
-                ),
+                _buildNameField(),
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                _buildSubmitButton(),
               ],
             ),
           ),
