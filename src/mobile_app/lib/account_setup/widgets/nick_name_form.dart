48071a34cc60bc295fe485c015895e0ff3a299ca
HRahimy <rahimy.hamza@gmail.com>
2021-05-13 16:55:37 +0300

    Add nickName form component hierarchy

--- src/mobile_app/lib/account_setup/widgets/nick_name_form.dart
+++ src/mobile_app/lib/account_setup/widgets/nick_name_form.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/positive_affirmations_keys.dart';
 
 class NickNameForm extends StatelessWidget {
   static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Text('NickNameFormWorks!');
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 35),
+      child: Form(
+        key: _formKey,
+        child: Align(
+          alignment: Alignment.center,
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              RichText(
+                key: PositiveAffirmationsKeys.nickNameFieldLabel,
+                text: TextSpan(text: 'label'),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              TextField(
+                key: PositiveAffirmationsKeys.nickNameField,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              ElevatedButton(
+                key: PositiveAffirmationsKeys.nickNameSubmitButton,
+                onPressed: () {},
+                child: Text('NEXT'),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              OutlinedButton(
+                key: PositiveAffirmationsKeys.changeNameButton,
+                onPressed: () {},
+                child: Text('BACK'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
