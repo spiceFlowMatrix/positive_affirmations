6b8872871948e43c01395863d5281843961fdb4b
HRahimy <rahimy.hamza@gmail.com>
2021-05-19 02:46:32 +0300

    Setup app summary screen tests

---
+++ src/mobile_app/test/account_setup/widgets/app_summary_screen_test.dart
@@ -0,0 +1,29 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile_app/account_setup/blocs/sign_up/sign_up_bloc.dart';
+import 'package:mobile_app/account_setup/models/models.dart';
+import 'package:mocktail/mocktail.dart';
+
+class FakeSignUpEvent extends Fake implements SignUpEvent {}
+
+class FakeSignUpState extends Fake implements SignUpState {}
+
+class MockSignUpBloc extends MockBloc<SignUpEvent, SignUpState>
+    implements SignUpBloc {}
+
+class MockNameField extends Mock implements NameField {}
+
+void main() {
+  group('[AppSummaryScreen]', () {
+    // late SignUpBloc signUpBloc;
+
+    setUpAll(() {
+      registerFallbackValue<SignUpEvent>(FakeSignUpEvent());
+      registerFallbackValue<SignUpState>(FakeSignUpState());
+    });
+
+    setUp(() {
+      // signUpBloc = MockSignUpBloc();
+    });
+  });
+}
