019f5da72cef23cfbd8facf5389a9fbf40112f07
HRahimy <rahimy.hamza@gmail.com>
2021-06-29 13:50:18 +0300

    Add object transformers for affirmation

--- src/mobile_app/lib/models/affirmation.dart
+++ src/mobile_app/lib/models/affirmation.dart
@@ -17,4 +17,45 @@ class Affirmation extends Equatable {
 
   @override
   List<Object> get props => [id, title, subtitle, likes, totalReaffirmations];
+
+  static const empty = Affirmation(id: '-', title: '-', subtitle: '-');
+  static const String fieldId = 'id';
+  static const String fieldTitle = 'title';
+  static const String fieldSubtitle = 'subtitle';
+  static const String fieldLikes = 'likes';
+  static const String fieldTotalReaffirmations = 'totalReaffirmations';
+
+  Map<String, dynamic> get fieldValues => {
+        fieldId: this.id,
+        fieldTitle: this.title,
+        fieldSubtitle: this.subtitle,
+        fieldLikes: this.likes,
+        fieldTotalReaffirmations: this.totalReaffirmations,
+      };
+
+  static Affirmation fromJson(Map<String, dynamic> json) {
+    return Affirmation(
+      id: json[Affirmation.fieldId] ?? '',
+      title: json[Affirmation.fieldTitle] ?? '',
+      subtitle: json[Affirmation.fieldSubtitle] ?? '',
+      likes: json[Affirmation.fieldLikes] ?? 0,
+      totalReaffirmations: json[Affirmation.fieldTotalReaffirmations] ?? 0,
+    );
+  }
+
+  Affirmation copyWith({
+    String? id,
+    String? title,
+    String? subtitle,
+    int? likes,
+    int? totalReaffirmations,
+  }) {
+    return Affirmation(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      subtitle: subtitle ?? this.subtitle,
+      likes: likes ?? this.likes,
+      totalReaffirmations: totalReaffirmations ?? this.totalReaffirmations,
+    );
+  }
 }
