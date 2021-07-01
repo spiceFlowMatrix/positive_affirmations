9bc2c6375798dfd441467c9b3dc3025520f1f25e
HRahimy <rahimy.hamza@gmail.com>
2021-07-01 19:30:28 +0300

    Affirmation form screen boilerplate

---
+++ src/mobile_app/lib/affirmations/widgets/affirmation_form_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AffirmationFormScreen extends StatelessWidget {
+  static const String routeName = '/affirmationFormScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
