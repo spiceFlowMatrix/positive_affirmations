37bbb887671223f8c29c05ca15c9f5d7270450cb
HRahimy <rahimy.hamza@gmail.com>
2021-07-02 15:31:37 +0300

    Set up boilerplate for testing affirmation form screen

--- src/mobile_app/test/affirmations/widgets/affirmation_form_screen_test.dart
+++ src/mobile_app/test/affirmations/widgets/affirmation_form_screen_test.dart
@@ -3,6 +3,11 @@ import 'package:mobile_app/affirmations/blocs/affirmation_form/affirmation_form_
 import 'package:mobile_app/affirmations/blocs/affirmations/affirmations_bloc.dart';
 import 'package:mobile_app/consts.dart';
 import 'package:mobile_app/models/affirmation.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../mocks/affirmation_form_bloc_mock.dart';
+import '../../mocks/affirmations_bloc_mock.dart';
+import '../fixtures/affirmation_form_screen_fixture.dart';
 
 void main() {
   late AffirmationsBloc affirmationsBloc;
@@ -10,5 +15,22 @@ void main() {
   final Affirmation toUpdateAffirmation =
       PositiveAffirmationsConsts.seedAffirmations[1];
 
-  group('[AffirmationFormScreen]', () {});
+  setUpAll(() {
+    registerFallbackValue<AffirmationsState>(FakeAffirmationsState());
+    registerFallbackValue<AffirmationsEvent>(FakeAffirmationsEvent());
+    registerFallbackValue<AffirmationFormState>(FakeAffirmationFormState());
+    registerFallbackValue<AffirmationFormEvent>(FakeAffirmationFormEvent());
+  });
+
+  setUp(() {
+    affirmationsBloc = MockAffirmationsBloc();
+    affirmationFormBloc = MockAffirmationFormBloc();
+  });
+
+  group('[AffirmationFormScreen]', () {
+    testWidgets('all components exist', (tester) async {
+      await tester.pumpWidget(
+          AffirmationFormScreenFixture(affirmationsBloc: affirmationsBloc));
+    });
+  });
 }
