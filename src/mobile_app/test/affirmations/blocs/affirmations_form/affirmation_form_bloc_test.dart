6e3addfc9b0014cf262bb47ba3c7c1b0ed708431
HRahimy <rahimy.hamza@gmail.com>
2021-07-02 13:54:03 +0300

    Add constructor test for affirmations form bloc

---
+++ src/mobile_app/test/affirmations/blocs/affirmations_form/affirmation_form_bloc_test.dart
@@ -0,0 +1,69 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:formz/formz.dart';
+import 'package:mobile_app/affirmations/blocs/affirmation_form/affirmation_form_bloc.dart';
+import 'package:mobile_app/affirmations/models/subtitle_field.dart';
+import 'package:mobile_app/affirmations/models/title_field.dart';
+
+/// 2. valid title supplied in TitleUpdated emits new state
+/// 3. invalid OR empty title supplied in TitleUpdated does not emit new state
+/// 4. valid subtitle supplied in SubtitleUpdated emits new state
+/// 5. invalid subtitle supplied in SubtitleUpdated does not emit new state
+/// 7. submit event emits new state only if form is valid
+
+void main() {
+  const String mockValidTitle = 'valid title';
+  const String mockInvalidTitle = 'fwef47*';
+  const String mockValidSubtitle = 'valid subtitle';
+  const String mockInvalidSubtitle = 'invalid-subtitle**';
+
+  late AffirmationFormBloc formBloc;
+  late AffirmationFormBloc titleInitFormBloc;
+  late AffirmationFormBloc subtitleInitFormBloc;
+
+  setUp(() {
+    formBloc = AffirmationFormBloc();
+    titleInitFormBloc = AffirmationFormBloc(
+      initialTitle: mockValidTitle,
+    );
+    subtitleInitFormBloc = AffirmationFormBloc(
+      initialSubtitle: mockValidSubtitle,
+    );
+  });
+
+  group('[AffirmationFormBloc]', () {
+    test('initial state is AffirmationFormState', () {
+      expect(formBloc.state, AffirmationFormState());
+    });
+    test('initial state supplied dirty title', () {
+      expect(
+        titleInitFormBloc.state,
+        AffirmationFormState(title: TitleField.dirty(mockValidTitle)),
+      );
+    });
+    test('initial state supplied dirty subtitle', () {
+      expect(
+        subtitleInitFormBloc.state,
+        AffirmationFormState(
+          subtitle: SubtitleField.dirty(mockValidSubtitle),
+        ),
+      );
+    });
+
+    group('[TitleUpdated]', () {
+      blocTest<AffirmationFormBloc, AffirmationFormState>(
+        'supplying valid title emits new state',
+        build: () => formBloc,
+        act: (bloc) {
+          bloc..add(TitleUpdated(mockValidTitle));
+        },
+        expect: () => <AffirmationFormState>[
+          const AffirmationFormState(
+            title: TitleField.dirty(mockValidTitle),
+            status: FormzStatus.valid,
+          ),
+        ],
+      );
+    });
+  });
+}
