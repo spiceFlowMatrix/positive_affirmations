b53ac3249052f9ff547a8649a6f5c6b2e2d69f4c
HRahimy <rahimy.hamza@gmail.com>
2021-07-04 14:42:52 +0300

    Compose affirmation detail screen

--- src/mobile_app/lib/affirmations/widgets/affirmation_detail_screen.dart
+++ src/mobile_app/lib/affirmations/widgets/affirmation_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mobile_app/affirmations/widgets/likes_span.dart';
 import 'package:mobile_app/models/affirmation.dart';
 import 'package:mobile_app/positive_affirmations_keys.dart';
 
@@ -29,6 +30,14 @@ class AffirmationDetailScreen extends StatelessWidget {
           key: PositiveAffirmationsKeys.affirmationDetailsBackButton(
               '${args.affirmation.id}'),
         ),
+        actions: [
+          IconButton(
+            key: PositiveAffirmationsKeys.affirmationDetailsAppbarEditButton(
+                '${args.affirmation.id}'),
+            onPressed: () {},
+            icon: FaIcon(FontAwesomeIcons.pen),
+          ),
+        ],
       ),
       body: _Body(args.affirmation),
     );
@@ -45,7 +54,41 @@ class _Body extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 35),
       child: Column(
-        children: [],
+        children: [
+          Text(
+            affirmation.title,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+            key: PositiveAffirmationsKeys.affirmationDetailsTitle(
+                '${affirmation.id}'),
+          ),
+          Text(
+            affirmation.subtitle,
+            style: TextStyle(),
+            key: PositiveAffirmationsKeys.affirmationDetailsSubtitle(
+                '${affirmation.id}'),
+          ),
+          LikesSpan(
+            affirmation,
+            spanKey: PositiveAffirmationsKeys.affirmationDetailsLikes(
+                '${affirmation.id}'),
+            likeButtonKey:
+                PositiveAffirmationsKeys.affirmationDetailsLikeButton(
+                    '${affirmation.id}'),
+          ),
+          Text(
+            '${affirmation.totalReaffirmations} reaffirmations',
+            key: PositiveAffirmationsKeys.affirmationDetailsReaffirmationsCount(
+                '${affirmation.id}'),
+          ),
+          ElevatedButton(
+            key: PositiveAffirmationsKeys.affirmationDetailsReaffirmButton(
+                '${affirmation.id}'),
+            onPressed: () {},
+            child: Text('REAFFIRM'),
+          ),
+        ],
       ),
     );
   }
