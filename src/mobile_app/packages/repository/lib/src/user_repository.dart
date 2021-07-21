1517a6b971ccbd627ca15db40ba6df8b9913ce8b
HRahimy <rahimy.hamza@gmail.com>
2021-07-21 17:20:22 +0300

    Add user creation function for repository

--- src/mobile_app/packages/repository/lib/src/user_repository.dart
+++ src/mobile_app/packages/repository/lib/src/user_repository.dart
@@ -1,1 +1,15 @@
-class UserRepository {}
+import 'package:repository/src/models/models.dart';
+import 'package:uuid/uuid.dart';
+
+class UserRepository {
+  Future<User?> createUser(String name, String? nickName) async {
+    return Future.delayed(
+      const Duration(milliseconds: 500),
+      () => User(
+        id: Uuid().v4(),
+        name: name,
+        nickName: nickName ?? '',
+      ),
+    );
+  }
+}
