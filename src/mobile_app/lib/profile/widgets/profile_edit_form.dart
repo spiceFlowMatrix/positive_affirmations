32f96e9997b0710bd947591169ddd211d9182493
HRahimy <rahimy.hamza@gmail.com>
2021-07-24 15:16:22 +0300

    Fix fixture dependency injection for blocs

--- src/mobile_app/lib/profile/widgets/profile_edit_form.dart
+++ src/mobile_app/lib/profile/widgets/profile_edit_form.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_app/positive_affirmations_keys.dart';
 import 'package:mobile_app/profile/blocs/profile/profile_bloc.dart';
-import 'package:repository/repository.dart';
+import 'package:mobile_app/profile/blocs/profile_edit/profile_edit_bloc.dart';
 
 class ProfileEditFormArgs {
   const ProfileEditFormArgs({
     required this.profileBloc,
+    this.profileEditBloc,
   });
 
   final ProfileBloc profileBloc;
+  final ProfileEditBloc? profileEditBloc;
 }
 
 class ProfileEditForm extends StatelessWidget {
@@ -20,12 +24,30 @@ class ProfileEditForm extends StatelessWidget {
     final args =
         ModalRoute.of(context)!.settings.arguments as ProfileEditFormArgs;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Profile'),
-      ),
-      body: Center(
-        child: Text('Profile edit form'),
+    final profileEditBloc = args.profileEditBloc == null
+        ? BlocProvider(
+            create: (_) => ProfileEditBloc(
+              profileBloc: args.profileBloc,
+            ),
+          )
+        : BlocProvider<ProfileEditBloc>.value(value: args.profileEditBloc!);
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ProfileBloc>.value(value: args.profileBloc),
+        profileEditBloc,
+      ],
+      child: Scaffold(
+        key: PositiveAffirmationsKeys.profileEditScreen,
+        appBar: AppBar(
+          title: Text(
+            'Edit Profile',
+            key: PositiveAffirmationsKeys.profileEditScreenTitle,
+          ),
+        ),
+        body: Center(
+          child: Text('Profile edit form'),
+        ),
       ),
     );
   }
--- src/mobile_app/test/profile/fixtures/profile_edit_form_fixture.dart
+++ src/mobile_app/test/profile/fixtures/profile_edit_form_fixture.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile_app/profile/blocs/profile/profile_bloc.dart';
 import 'package:mobile_app/profile/blocs/profile_edit/profile_edit_bloc.dart';
 import 'package:mobile_app/profile/widgets/profile_edit_form.dart';
@@ -15,29 +14,24 @@ class ProfileEditFormFixture extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ProfileBloc>.value(value: profileBloc),
-        BlocProvider<ProfileEditBloc>.value(value: profileEditBloc),
-      ],
-      child: MaterialApp(
-        onGenerateRoute: (settings) {
-          final args = ProfileEditFormArgs(
-            profileBloc: profileBloc,
-          );
+    return MaterialApp(
+      onGenerateRoute: (settings) {
+        final args = ProfileEditFormArgs(
+          profileBloc: profileBloc,
+          profileEditBloc: profileEditBloc,
+        );
 
-          return MaterialPageRoute(
-            builder: (context) {
-              return ProfileEditForm();
-            },
-            settings: RouteSettings(
-              name: ProfileEditForm.routeName,
-              arguments: args,
-            ),
-          );
-        },
-        initialRoute: ProfileEditForm.routeName,
-      ),
+        return MaterialPageRoute(
+          builder: (context) {
+            return ProfileEditForm();
+          },
+          settings: RouteSettings(
+            name: ProfileEditForm.routeName,
+            arguments: args,
+          ),
+        );
+      },
+      initialRoute: ProfileEditForm.routeName,
     );
   }
 }
