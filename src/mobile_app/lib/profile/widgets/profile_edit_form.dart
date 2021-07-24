eddfacaa1ce47759935c60f7fb298b6aaffac8ac
HRahimy <rahimy.hamza@gmail.com>
2021-07-24 15:45:34 +0300

    Add profile edit form fields

--- src/mobile_app/lib/profile/widgets/profile_edit_form.dart
+++ src/mobile_app/lib/profile/widgets/profile_edit_form.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_app/consts.dart';
 import 'package:mobile_app/positive_affirmations_keys.dart';
 import 'package:mobile_app/profile/blocs/profile/profile_bloc.dart';
 import 'package:mobile_app/profile/blocs/profile_edit/profile_edit_bloc.dart';
+import 'package:repository/repository.dart';
 
 class ProfileEditFormArgs {
   const ProfileEditFormArgs({
@@ -45,10 +47,94 @@ class ProfileEditForm extends StatelessWidget {
             key: PositiveAffirmationsKeys.profileEditScreenTitle,
           ),
         ),
-        body: Center(
-          child: Text('Profile edit form'),
+        body: _FormContent(userInitial: args.profileBloc.state.user),
+      ),
+    );
+  }
+}
+
+class _FormContent extends StatelessWidget {
+  _FormContent({required this.userInitial})
+      : super(key: PositiveAffirmationsKeys.profileEditForm(userInitial.id));
+
+  final GlobalKey<FormState> _editProfileFormKey = GlobalKey<FormState>();
+
+  final User userInitial;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Form(
+        key: _editProfileFormKey,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 35),
+          shrinkWrap: true,
+          children: [
+            _FormField(
+              fieldLabel: PositiveAffirmationsConsts.profileEditNameFieldLabel,
+              fieldLabelKey: PositiveAffirmationsKeys.profileEditNameFieldLabel,
+              fieldInputLabel: 'Name',
+              fieldKey:
+                  PositiveAffirmationsKeys.profileEditNameField(userInitial.id),
+              initialName: userInitial.name,
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20)),
+            _FormField(
+              fieldLabel:
+                  PositiveAffirmationsConsts.profileEditNickNameFieldLabel,
+              fieldLabelKey:
+                  PositiveAffirmationsKeys.profileEditNickNameFieldLabel,
+              fieldInputLabel: 'Nickname',
+              fieldKey: PositiveAffirmationsKeys.profileEditNickNameField(
+                  userInitial.id),
+              initialName: userInitial.nickName,
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class _FormField extends StatelessWidget {
+  _FormField({
+    required this.fieldLabel,
+    required this.fieldLabelKey,
+    required this.fieldKey,
+    required this.fieldInputLabel,
+    required this.initialName,
+  }) : _textEditingController = TextEditingController(text: initialName);
+
+  final String fieldLabel;
+  final Key fieldLabelKey;
+  final Key fieldKey;
+  final String fieldInputLabel;
+  final String initialName;
+  final TextEditingController _textEditingController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          fieldLabel,
+          key: fieldLabelKey,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const Padding(padding: EdgeInsets.only(top: 10)),
+        TextField(
+          key: fieldKey,
+          controller: _textEditingController,
+          onChanged: (value) {},
+          decoration: InputDecoration(
+            labelText: fieldInputLabel,
+          ),
+        ),
+      ],
     );
   }
 }
