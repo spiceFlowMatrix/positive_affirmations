ed5beae1766497e38a3c7654c4d3a8b43615c3e3
HRahimy <rahimy.hamza@gmail.com>
2021-10-06 18:47:42 +0300

    Letters feature Local notifications plugin setup

--- src/mobile_app/lib/main.dart
+++ src/mobile_app/lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mobile_app/app.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:repository/repository.dart';
 
+void selectNotification(String? payload) async {
+  if (payload != null) {
+    debugPrint('notification payload: $payload');
+  }
+}
+
 void main() async {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+  final InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: selectNotification);
+
   WidgetsFlutterBinding.ensureInitialized();
   final documentsDirectory = await getApplicationDocumentsDirectory();
   HydratedBloc.storage =
