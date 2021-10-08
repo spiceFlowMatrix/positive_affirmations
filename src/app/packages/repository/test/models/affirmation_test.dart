56a2ac2cd6bf0accf7734d8eae0473bbccf3a3ce
HRahimy <rahimy.hamza@gmail.com>
2021-10-08 18:27:06 +0300

    Flutter project version migration Add repository package

---
+++ src/app/packages/repository/test/models/affirmation_test.dart
@@ -0,0 +1,98 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:repository/repository.dart';
+
+void main() {
+  Affirmation mockValidAffirmation = Affirmation(
+    id: 0,
+    title: 'testTitle',
+    subtitle: 'testSubtitle',
+    createdById: PositiveAffirmationsRepositoryConsts.seedUser.id,
+    createdOn: DateTime.now(),
+    totalReaffirmations: 12,
+    likes: 8,
+    active: true,
+  );
+
+  Map<String, dynamic> mockValidAffirmationJson = {
+    Affirmation.fieldId: mockValidAffirmation.id,
+    Affirmation.fieldSubtitle: mockValidAffirmation.subtitle,
+    Affirmation.fieldTitle: mockValidAffirmation.title,
+    Affirmation.fieldCreatedById: mockValidAffirmation.createdById,
+    Affirmation.fieldCreatedOn:
+        mockValidAffirmation.createdOn.toIso8601String(),
+    Affirmation.fieldLikes: mockValidAffirmation.likes,
+    Affirmation.fieldTotalReaffirmations:
+        mockValidAffirmation.totalReaffirmations,
+    Affirmation.fieldActive: mockValidAffirmation.active,
+    Affirmation.fieldLiked: mockValidAffirmation.liked,
+  };
+
+  group('[AffirmationModel]', () {
+    group('json_converters', () {
+      test('fieldValues create valid json object', () {
+        expect(
+          mockValidAffirmation.fieldValues,
+          equals(mockValidAffirmationJson),
+        );
+      });
+
+      test('valid fromJson creates a valid Affirmation object', () {
+        expect(
+          Affirmation.fromJson(mockValidAffirmationJson),
+          equals(mockValidAffirmation),
+        );
+      });
+
+      test('missing fields in a json object are handled', () {
+        expect(
+          Affirmation.fromJson({Affirmation.fieldId: mockValidAffirmation.id}),
+          equals(Affirmation.empty.copyWith(id: mockValidAffirmation.id)),
+        );
+        expect(
+          Affirmation.fromJson(
+              {Affirmation.fieldTitle: mockValidAffirmation.title}),
+          equals(Affirmation.empty.copyWith(title: mockValidAffirmation.title)),
+        );
+        expect(
+          Affirmation.fromJson(
+              {Affirmation.fieldSubtitle: mockValidAffirmation.subtitle}),
+          equals(Affirmation.empty
+              .copyWith(subtitle: mockValidAffirmation.subtitle)),
+        );
+        expect(
+          Affirmation.fromJson(
+              {Affirmation.fieldLikes: mockValidAffirmation.likes}),
+          equals(Affirmation.empty.copyWith(likes: mockValidAffirmation.likes)),
+        );
+        expect(
+          Affirmation.fromJson({
+            Affirmation.fieldTotalReaffirmations:
+                mockValidAffirmation.totalReaffirmations
+          }),
+          equals(Affirmation.empty.copyWith(
+              totalReaffirmations: mockValidAffirmation.totalReaffirmations)),
+        );
+
+        expect(
+          Affirmation.fromJson(
+              {Affirmation.fieldCreatedById: mockValidAffirmation.createdById}),
+          equals(Affirmation.empty
+              .copyWith(createdById: mockValidAffirmation.createdById)),
+        );
+
+        expect(
+            Affirmation.fromJson(
+                {Affirmation.fieldCreatedOn: mockValidAffirmation.createdOn}),
+            equals(Affirmation.empty.copyWith(
+              createdOn: mockValidAffirmation.createdOn,
+            )));
+
+        expect(
+          Affirmation.fromJson(
+              {Affirmation.fieldLiked: mockValidAffirmation.liked}),
+          equals(Affirmation.empty.copyWith(liked: mockValidAffirmation.liked)),
+        );
+      });
+    });
+  });
+}
---
+++ src/app/packages/repository/test/models/reaffirmation_test.dart
@@ -0,0 +1,176 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:repository/repository.dart';
+
+void main() {
+  final createdOn = DateTime.now();
+  createdOn.add(Duration(days: 5));
+  Reaffirmation mockValidReaffirmation = Reaffirmation(
+    id: 1,
+    affirmationId: 1,
+    createdOn: createdOn,
+    value: ReaffirmationValue.goodWork,
+    font: ReaffirmationFont.montserrat,
+    stamp: ReaffirmationStamp.medal,
+  );
+
+  Map<String, dynamic> mockValidReaffirmationsJson = {
+    Reaffirmation.fieldId: mockValidReaffirmation.id,
+    Reaffirmation.fieldAffirmationId: mockValidReaffirmation.affirmationId,
+    Reaffirmation.fieldCreatedOn:
+        mockValidReaffirmation.createdOn.toIso8601String(),
+    Reaffirmation.fieldValue: mockValidReaffirmation.value.index,
+    Reaffirmation.fieldFont: mockValidReaffirmation.font.index,
+    Reaffirmation.fieldStamp: mockValidReaffirmation.stamp.index
+  };
+
+  group('[ReaffirmationModel]', () {
+    group('[json_converters]', () {
+      test('fieldValues create valid json object', () {
+        expect(
+          mockValidReaffirmation.fieldValues,
+          equals(mockValidReaffirmationsJson),
+        );
+      });
+
+      test('valid json creates a valid Reaffirmation object', () {
+        expect(
+          Reaffirmation.fromJson(mockValidReaffirmationsJson),
+          equals(mockValidReaffirmation),
+        );
+      });
+
+      test('missing fields in a json object are handled', () {
+        expect(
+          Reaffirmation.fromJson(
+              {Reaffirmation.fieldId: mockValidReaffirmation.id}),
+          Reaffirmation.empty.copyWith(id: mockValidReaffirmation.id),
+        );
+
+        expect(
+          Reaffirmation.fromJson({
+            Reaffirmation.fieldAffirmationId:
+            mockValidReaffirmation.affirmationId
+          }),
+          Reaffirmation.empty
+              .copyWith(affirmationId: mockValidReaffirmation.affirmationId),
+        );
+
+        expect(
+          Reaffirmation.fromJson(
+              {Reaffirmation.fieldCreatedOn: mockValidReaffirmation.createdOn}),
+          Reaffirmation.empty
+              .copyWith(createdOn: mockValidReaffirmation.createdOn),
+        );
+
+        expect(
+          Reaffirmation.fromJson(
+              {Reaffirmation.fieldValue: mockValidReaffirmation.value.index}),
+          Reaffirmation.empty.copyWith(value: mockValidReaffirmation.value),
+        );
+
+        expect(
+          Reaffirmation.fromJson(
+              {Reaffirmation.fieldFont: mockValidReaffirmation.font.index}),
+          Reaffirmation.empty.copyWith(font: mockValidReaffirmation.font),
+        );
+
+        expect(
+          Reaffirmation.fromJson(
+              {Reaffirmation.fieldStamp: mockValidReaffirmation.stamp.index}),
+          Reaffirmation.empty.copyWith(stamp: mockValidReaffirmation.stamp),
+        );
+      });
+    });
+
+    group('[copyWith]', () {
+      test('returns same object if no params passed', () {
+        expect(
+          Reaffirmation.empty.copyWith(),
+          equals(Reaffirmation.empty),
+        );
+      });
+      test('returns object with updated id if id is passed', () {
+        expect(
+          Reaffirmation.empty.copyWith(id: mockValidReaffirmation.id),
+          equals(Reaffirmation(
+            id: mockValidReaffirmation.id,
+            affirmationId: Reaffirmation.empty.affirmationId,
+            value: Reaffirmation.empty.value,
+            createdOn: Reaffirmation.empty.createdOn,
+            font: Reaffirmation.empty.font,
+            stamp: Reaffirmation.empty.stamp,
+          )),
+        );
+      });
+      test(
+          'returns object with updated affirmationId if affirmationId is passed',
+          () {
+        expect(
+          Reaffirmation.empty
+              .copyWith(affirmationId: mockValidReaffirmation.affirmationId),
+          equals(Reaffirmation(
+            id: Reaffirmation.empty.id,
+            affirmationId: mockValidReaffirmation.affirmationId,
+            value: Reaffirmation.empty.value,
+            createdOn: Reaffirmation.empty.createdOn,
+            font: Reaffirmation.empty.font,
+            stamp: Reaffirmation.empty.stamp,
+          )),
+        );
+      });
+      test('returns object with updated value if value is passed', () {
+        expect(
+          Reaffirmation.empty.copyWith(value: mockValidReaffirmation.value),
+          equals(Reaffirmation(
+            id: Reaffirmation.empty.id,
+            affirmationId: Reaffirmation.empty.affirmationId,
+            value: mockValidReaffirmation.value,
+            createdOn: Reaffirmation.empty.createdOn,
+            font: Reaffirmation.empty.font,
+            stamp: Reaffirmation.empty.stamp,
+          )),
+        );
+      });
+      test('returns object with updated createdOn if createdOn is passed', () {
+        expect(
+          Reaffirmation.empty
+              .copyWith(createdOn: mockValidReaffirmation.createdOn),
+          equals(Reaffirmation(
+            id: Reaffirmation.empty.id,
+            affirmationId: Reaffirmation.empty.affirmationId,
+            value: Reaffirmation.empty.value,
+            createdOn: mockValidReaffirmation.createdOn,
+            font: Reaffirmation.empty.font,
+            stamp: Reaffirmation.empty.stamp,
+          )),
+        );
+      });
+      test('returns object with updated font if font is passed', () {
+        expect(
+          Reaffirmation.empty.copyWith(font: mockValidReaffirmation.font),
+          equals(Reaffirmation(
+            id: Reaffirmation.empty.id,
+            affirmationId: Reaffirmation.empty.affirmationId,
+            value: Reaffirmation.empty.value,
+            createdOn: Reaffirmation.empty.createdOn,
+            font: mockValidReaffirmation.font,
+            stamp: Reaffirmation.empty.stamp,
+          )),
+        );
+      });
+      test('returns object with updated stamp if stamp is passed', () {
+        expect(
+          Reaffirmation.empty.copyWith(stamp: mockValidReaffirmation.stamp),
+          equals(Reaffirmation(
+            id: Reaffirmation.empty.id,
+            affirmationId: Reaffirmation.empty.affirmationId,
+            value: Reaffirmation.empty.value,
+            createdOn: Reaffirmation.empty.createdOn,
+            font: Reaffirmation.empty.font,
+            stamp: mockValidReaffirmation.stamp,
+          )),
+        );
+      });
+    });
+  });
+}
