3976f751ed1723a02d998f369daff1db3cbe186a
HRahimy <rahimy.hamza@gmail.com>
2021-10-22 01:29:35 +0300

    Account setup rework Update nick name test fixture to work with reworked navigation workflow

--- src/app/test/account_setup/fixtures/nick_name_form_fixture.dart
+++ src/app/test/account_setup/fixtures/nick_name_form_fixture.dart
@@ -1,24 +1,53 @@
+import 'package:app/account_setup/blocs/sign_up/sign_up_bloc.dart';
+import 'package:app/account_setup/widgets/sign_up_flow.dart';
+import 'package:app/blocs/authentication/authentication_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:app/account_setup/blocs/sign_up/sign_up_bloc.dart';
-import 'package:app/account_setup/widgets/nick_name_form.dart';
-import 'package:app/positive_affirmations_routes.dart';
+import 'package:repository/repository.dart';
 
 class NickNameFormFixture extends StatelessWidget {
-  NickNameFormFixture(this.bloc);
+  const NickNameFormFixture(
+    this.bloc, {
+    Key? key,
+    required this.userRepository,
+    this.navigatorObserver,
+    this.authBloc,
+  }) : super(key: key);
 
   final SignUpBloc bloc;
+  final UserRepository userRepository;
+  final AuthenticationBloc? authBloc;
+  final NavigatorObserver? navigatorObserver;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: bloc,
-      child: MaterialApp(
-        home: Scaffold(
-          body: NickNameForm(),
+    return RepositoryProvider.value(
+      value: userRepository,
+      child: MultiBlocProvider(
+        providers: [
+          if (authBloc != null)
+            BlocProvider<AuthenticationBloc>.value(value: authBloc!),
+          BlocProvider<SignUpBloc>.value(value: bloc),
+        ],
+        child: MaterialApp(
+          initialRoute: SignUpFlow.routeName,
+          navigatorObservers: [
+            if (navigatorObserver != null) navigatorObserver!
+          ],
+          onGenerateRoute: (settings) {
+            final args = SignUpFlowArgs(signUpBloc: bloc);
+
+            return MaterialPageRoute(
+              builder: (context) {
+                return const SignUpFlow();
+              },
+              settings: RouteSettings(
+                name: SignUpFlow.routeName,
+                arguments: args,
+              ),
+            );
+          },
         ),
-        // initialRoute: NameFormScreen.routeName,
-        routes: PositiveAffirmationsRoutes().routes(context),
       ),
     );
   }
