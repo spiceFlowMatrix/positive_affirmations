22d457cfa21df8cc8d4230cff6857e29cf158323
HRahimy <rahimy.hamza@gmail.com>
2021-10-21 22:49:14 +0300

    Account setup rework Remove dummy button implementation

--- src/app/lib/account_setup/widgets/name_screen.dart
+++ src/app/lib/account_setup/widgets/name_screen.dart
@@ -1,8 +1,4 @@
-import 'package:app/account_setup/blocs/sign_up/sign_up_bloc.dart';
-import 'package:app/blocs/authentication/authentication_bloc.dart';
-import 'package:app/profile/blocs/profile/profile_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NameScreen extends StatelessWidget {
   const NameScreen({Key? key}) : super(key: key);
@@ -17,23 +13,7 @@ class NameScreen extends StatelessWidget {
           children: [
             const Text('Name Form Screen'),
             ElevatedButton(
-              onPressed: () {
-                final bloc = BlocProvider.of<SignUpBloc>(context);
-                bloc.add(const NameUpdated('testName'));
-                bloc.add(const NameSubmitted());
-                bloc.add(const NickNameUpdated('testNickname'));
-                bloc.add(const NickNameSubmitted());
-                bloc.add(UserSubmitted());
-
-                final state = bloc.state;
-
-                BlocProvider.of<ProfileBloc>(context)
-                    .add(UserCreated(user: state.createdUser));
-                BlocProvider.of<AuthenticationBloc>(context)
-                    .add(const AuthenticationStatusChanged(
-                  status: AuthenticationStatus.authenticated,
-                ));
-              },
+              onPressed: () {},
               child: const Text('Submit'),
             ),
           ],
