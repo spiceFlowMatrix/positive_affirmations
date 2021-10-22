9ec9b1126db18c16c496ec1aec339dda999fa171
HRahimy <rahimy.hamza@gmail.com>
2021-10-23 01:48:04 +0300

    Account setup rework [Account details screen] Add password and confirm-password fields

--- src/app/lib/account_setup/widgets/account_details_form.dart
+++ src/app/lib/account_setup/widgets/account_details_form.dart
@@ -2,6 +2,7 @@ import 'package:app/account_setup/blocs/sign_up/sign_up_bloc.dart';
 import 'package:app/account_setup/widgets/already_have_account_content.dart';
 import 'package:app/consts.dart';
 import 'package:app/models/email_field.dart';
+import 'package:app/models/password_field.dart';
 import 'package:app/positive_affirmations_keys.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -36,6 +37,10 @@ class _Form extends StatelessWidget {
               _Label(),
               Padding(padding: EdgeInsets.only(top: 10)),
               _EmailField(),
+              Padding(padding: EdgeInsets.only(top: 10)),
+              _PasswordField(),
+              Padding(padding: EdgeInsets.only(top: 10)),
+              _ConfirmPasswordField(),
               Padding(padding: EdgeInsets.only(top: 10)),
               _SubmitButton(),
               Padding(padding: EdgeInsets.only(top: 10)),
@@ -107,6 +112,66 @@ class _EmailField extends StatelessWidget {
   }
 }
 
+class _PasswordField extends StatelessWidget {
+  const _PasswordField();
+
+  String _generateErrorText(PasswordFieldValidationError error) {
+    switch (error) {
+      case PasswordFieldValidationError.empty:
+        return PositiveAffirmationsConsts.invalidEmailErrorText;
+      case PasswordFieldValidationError.invalid_password:
+        return PositiveAffirmationsConsts.invalidEmailErrorText;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignUpBloc, SignUpState>(
+      builder: (context, state) {
+        return TextField(
+          key: PositiveAffirmationsKeys.nameField,
+          onChanged: (password) => context
+              .read<SignUpBloc>()
+              .add(PasswordUpdated(password: password)),
+          keyboardType: TextInputType.visiblePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            errorText: state.password.invalid
+                ? _generateErrorText(state.password.error!)
+                : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ConfirmPasswordField extends StatelessWidget {
+  const _ConfirmPasswordField();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignUpBloc, SignUpState>(
+      builder: (context, state) {
+        return TextField(
+          key: PositiveAffirmationsKeys.nameField,
+          onChanged: (password) => context
+              .read<SignUpBloc>()
+              .add(ConfirmPasswordUpdated(confirmPassword: password)),
+          keyboardType: TextInputType.visiblePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Confirm password',
+            errorText:
+                state.confirmPassword.invalid ? 'Passwords do not match' : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
 class _SubmitButton extends StatelessWidget {
   const _SubmitButton();
 
@@ -116,12 +181,14 @@ class _SubmitButton extends StatelessWidget {
       builder: (context, state) {
         return ElevatedButton(
           key: PositiveAffirmationsKeys.nameSubmitButton,
-          onPressed:
-              state.emailStatus.isValid && state.emailStatus != FormzStatus.pure
-                  ? () {
-                      context.read<SignUpBloc>().add(AccountDetailsSubmitted());
-                    }
-                  : null,
+          onPressed: state.emailStatus.isValidated &&
+                  state.emailStatus != FormzStatus.pure &&
+                  state.passwordStatus.isValidated &&
+                  state.confirmPasswordStatus.isValidated
+              ? () {
+                  context.read<SignUpBloc>().add(AccountDetailsSubmitted());
+                }
+              : null,
           child: const Text('Done'),
         );
       },
