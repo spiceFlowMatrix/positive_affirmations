4956ce0d618e72788dbb8cb02f251c6350612c5b
HRahimy <rahimy.hamza@gmail.com>
2021-10-28 01:51:08 +0300

    Initialize firebase project

--- src/firebase/functions/src/index.ts
+++ src/firebase/functions/src/index.ts
@@ -1,4 +1,8 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+// const firestore = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +11,24 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// export interface User {
+//   id: string;
+//   name: string;
+//   nickName: string;
+//   pictureB64Enc: string;
+//   email: string;
+//   accountCreated: boolean;
+//   emailVerified: boolean;
+// }
+//
+// export const checkLetters = functions.pubsub.schedule("every 2 minutes")
+//     .onRun(async (context) => {
+//       const users = await firestore
+//           .collection("users")
+//           .get();
+//       users.docs.map((doc) => doc.data()).forEach((data) => {
+//         console.log(JSON.stringify(data));
+//       });
+//       return null;
+//     });
