c4354de72855a0886e7b44a065879da48e5213ab
HRahimy <rahimy.hamza@gmail.com>
2021-10-28 15:58:02 +0300

    Implement UI layout for profile letters tab

--- src/app/lib/profile/widgets/profile_letters_tab.dart
+++ src/app/lib/profile/widgets/profile_letters_tab.dart
@@ -1,12 +1,85 @@
+import 'package:app/profile/blocs/profile/profile_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:repository/repository.dart';
 
 class ProfileLettersTab extends StatelessWidget {
   const ProfileLettersTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile letters'),
+    return Column(
+      children: [
+        _OptionsButton(),
+        const Divider(
+          height: 0,
+          thickness: 1.5,
+        ),
+        Expanded(child: _List()),
+      ],
+    );
+  }
+}
+
+class _OptionsButton extends StatelessWidget {
+  String _generateTitleText(LetterCreationSchedule schedule) {
+    switch (schedule) {
+      case LetterCreationSchedule.daily:
+        return 'Daily';
+      case LetterCreationSchedule.weekly:
+        return 'Weekly';
+      case LetterCreationSchedule.monthly:
+        return 'Monthly';
+      case LetterCreationSchedule.never:
+        return 'Never';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProfileBloc, ProfileState>(
+      builder: (context, state) {
+        return ListTile(
+          onTap: () {},
+          title: Text(
+            _generateTitleText(state.user.letterSchedule),
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          subtitle: const Text(
+            'How often would you like me to check for letters?',
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          trailing: const FaIcon(
+            FontAwesomeIcons.chevronRight,
+            size: 15,
+            color: Colors.black,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _List extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        ListTile(
+          title: Text(
+            'Letters',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
