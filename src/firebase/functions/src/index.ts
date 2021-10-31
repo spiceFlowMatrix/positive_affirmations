168b18e36e8677899ab7169a6b4fee1d8d6b5365
HRahimy <rahimy.hamza@gmail.com>
2021-10-31 19:43:16 +0300

    Implement functional logic for generating letter data for all users

--- src/firebase/functions/src/index.ts
+++ src/firebase/functions/src/index.ts
@@ -2,15 +2,111 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 admin.initializeApp(functions.config().firebase);
-// const firestore = admin.firestore();
+const firestore = admin.firestore();
+
+export enum ReaffirmationValue { empty, braveOn, loveIt, goodWork }
+
+export enum ReaffirmationStamp { empty, takeOff, medal, thumbsUp }
+
+export enum ReaffirmationFont {
+    none,
+    birthstone,
+    gemunuLibre,
+    montserrat
+}
+
+export interface LetterAffirmationReaffirmation {
+    id: string;
+    createdOn: Date;
+    value: ReaffirmationValue;
+    font: ReaffirmationFont;
+    stamp: ReaffirmationStamp;
+}
+
+export interface LetterAffirmation {
+    id: string;
+    title: string;
+    subtitle: string;
+    totalReaffirmations: number;
+    reaffirmations: LetterAffirmationReaffirmation[];
+}
+
+export interface Letter {
+    id: string;
+    createdOn: Date;
+    totalAffirmations: number;
+    affirmations: LetterAffirmation[];
+}
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https
+    .onRequest(async (req, resp) => {
+      const users = await firestore.collection("users").get();
+      for (let i = 0; i < users.docs.length; i++) {
+        console.log(JSON.stringify(users.docs[i].data()));
+        const letterAffirmations = await
+        generateLetters(users.docs[i].data().id);
+        const newLetter = <Letter>{
+          id: makeid(15),
+          createdOn: new Date(Date.now()),
+          totalAffirmations: letterAffirmations.length,
+          affirmations: letterAffirmations,
+        };
+        await firestore.collection("users")
+            .doc(users.docs[i].data().id).collection("letters")
+            .doc(newLetter.id).set(newLetter);
+      }
+      resp.send("Success");
+    });
+
+export const makeid = (length: number): string => {
+  let result = "";
+  const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+  }
+  return result;
+};
+
+// Get all affirmations by given user
+// Get all reaffirmations for all the fetched affirmations
+// Create a new letter
+
+export const generateLetters = async (userId: string):
+    Promise<LetterAffirmation[]> => {
+  const affirmations = await firestore.collection("affirmations")
+      .where("createdById", "==", userId)
+      .get();
+  let letterAffirmations: LetterAffirmation[] = [];
+  for (let i = 0; i < affirmations.docs.length; i++) {
+    const id = affirmations.docs[i].data().id;
+    const reaffirmations = await firestore.collection("affirmations")
+        .doc(id).collection("reaffirmations").get().then((snap) => {
+          return snap.docs.map((doc) => doc.data());
+        });
+    letterAffirmations = [...letterAffirmations, <LetterAffirmation>{
+      id,
+      title: affirmations.docs[i].data().title,
+      subtitle: affirmations.docs[i].data().subtitle,
+      totalReaffirmations: reaffirmations.length,
+      reaffirmations: reaffirmations.map((e) => {
+        return <LetterAffirmationReaffirmation>{
+          id: e.id,
+          createdOn: e.createdOn,
+          font: e.font,
+          stamp: e.stamp,
+          value: e.value,
+        };
+      }),
+    }];
+  }
+  return letterAffirmations;
+};
 
 // export interface User {
 //   id: string;
