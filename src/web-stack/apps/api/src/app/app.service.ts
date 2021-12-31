ba0bef34d573460911253d5284b3b5a0b4301ba9
HRahimy <rahimy.hamza@gmail.com>
2021-12-31 03:22:18 +0300

    Re-init web stack workspace

--- src/web-stack/apps/api/src/app/app.service.ts
+++ src/web-stack/apps/api/src/app/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Message } from '@web-stack/api-interfaces';
 
 @Injectable()
 export class AppService {
-  getData(): Message {
+  getData(): { message: string } {
     return { message: 'Welcome to api!' };
   }
 }
