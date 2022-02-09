ef8c96a9a98dc5e189aaa235b327749ba0e811e5
HRahimy <rahimy.hamza@gmail.com>
2022-02-09 20:58:10 +0300

    Add in WIP skeletal test for sign-up.handler

---
+++ src/web-stack/libs/application/src/lib/command/handlers/sign-up.handler.spec.ts
@@ -0,0 +1,37 @@
+import {SignUpHandler} from "./sign-up.handler";
+import {Test} from "@nestjs/testing";
+import {CqrsModule} from "@nestjs/cqrs";
+import {UserEntity} from "@web-stack/domain";
+import {getRepositoryToken} from "@nestjs/typeorm";
+import {FirebaseAdminService} from "@web-stack/services";
+
+describe('SignUpHandler', () => {
+  let signUpHandler: SignUpHandler;
+  // let userRepository: Repository<UserEntity>;
+  // let firebaseAdminService: FirebaseAdminService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CqrsModule],
+      providers: [
+        {
+          // Solution for injecting typeorm entity repositories for tests:
+          // https://stackoverflow.com/a/58163804/5472560
+          provide: getRepositoryToken(UserEntity),
+          useFactory: () => jest.fn(),
+        },
+        {
+          provide: FirebaseAdminService,
+          useFactory: () => jest.fn(),
+        },
+        SignUpHandler
+      ],
+    }).compile();
+
+    signUpHandler = moduleRef.get<SignUpHandler>(SignUpHandler);
+  });
+
+  it('is defined', () => {
+    expect(signUpHandler).toBeDefined();
+  });
+});
