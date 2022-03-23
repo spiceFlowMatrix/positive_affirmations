1a06e00f7bb20d73da6603799ed2ffb281ff4ddf
HRahimy <rahimy.hamza@gmail.com>
2022-03-23 17:31:37 +0300

    Mobile app
    Implement automated navigation workflow between authenticated and unauthenticated states

--- src/app_rework/lib/app.dart
+++ src/app_rework/lib/app.dart
@@ -1,7 +1,11 @@
 import 'package:api_client/api_client.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:positive_affirmations/account/bloc/widgets/sign_up_form.dart';
 import 'package:positive_affirmations/common/blocs/app/app_bloc.dart';
+import 'package:positive_affirmations/home/widgets/home_screen.dart';
+import 'package:positive_affirmations/named_routes.dart';
+import 'package:positive_affirmations/theme.dart';
 import 'package:repository/repository.dart';
 
 class App extends StatelessWidget {
@@ -34,60 +38,64 @@ class App extends StatelessWidget {
                 AppBloc(authenticationRepository: _authenticationRepository),
           ),
         ],
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: const MyHomePage(title: 'Flutter Demo Home Page'),
-        ),
+        child: const _AppView(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class _AppView extends StatefulWidget {
+  const _AppView({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _AppViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AppViewState extends State<_AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  NavigatorState get _navigator => _navigatorKey.currentState!;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return BlocBuilder<AppBloc, AppState>(
+      builder: (context, state) {
+        return MaterialApp(
+          theme: AppTheme.theme,
+          navigatorKey: _navigatorKey,
+          builder: (context, child) {
+            return BlocListener<AppBloc, AppState>(
+              listener: (context, state) {
+                switch (state.status) {
+                  case AppStatus.authenticated:
+                    if (_navigator.widget.initialRoute !=
+                        HomeScreen.routeName) {
+                      _navigator.pushNamedAndRemoveUntil(
+                        HomeScreen.routeName,
+                        (route) => false,
+                      );
+                    }
+                    break;
+                  case AppStatus.unauthenticated:
+                    if (_navigator.widget.initialRoute !=
+                        SignUpForm.routeName) {
+                      _navigator.pushNamedAndRemoveUntil(
+                        SignUpForm.routeName,
+                        (route) => false,
+                      );
+                    }
+                    break;
+                }
+              },
+              child: child,
+            );
+          },
+          initialRoute: state.status == AppStatus.unauthenticated
+              ? SignUpForm.routeName
+              : HomeScreen.routeName,
+          routes: namedRoutes(context),
+        );
+      },
     );
   }
 }
--- src/app_rework/lib/named_routes.dart
+++ src/app_rework/lib/named_routes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:positive_affirmations/account/bloc/widgets/sign_up_form.dart';
+import 'package:positive_affirmations/home/widgets/home_screen.dart';
 
 Map<String, Widget Function(BuildContext context)> namedRoutes(
     BuildContext context) {
   return {
     SignUpForm.routeName: (context) => const SignUpForm(),
+    HomeScreen.routeName: (context) => const HomeScreen(),
   };
 }
