b0c1c3c300f63cb56a31f72369adaef0275542d4
HRahimy <rahimy.hamza@gmail.com>
2022-03-30 19:57:18 +0300

    Mobile app
    Implement name field UI

--- src/app_rework/lib/account/bloc/widgets/sign_up_form.dart
+++ src/app_rework/lib/account/bloc/widgets/sign_up_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:positive_affirmations/account/bloc/sign_up_form/sign_up_form_cubit.dart';
+import 'package:positive_affirmations/common/widgets/common_form_padding.dart';
 
 class SignUpForm extends StatelessWidget {
   const SignUpForm({Key? key}) : super(key: key);
@@ -7,9 +10,48 @@ class SignUpForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Sign up form'),
+    return BlocProvider<SignUpFormCubit>(
+      create: (_) => SignUpFormCubit(),
+      child: const Scaffold(
+        body: _Form(),
+      ),
+    );
+  }
+}
+
+class _Form extends StatelessWidget {
+  const _Form({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            _NameField(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NameField extends StatelessWidget {
+  const _NameField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.read<SignUpFormCubit>();
+    return CommonFormPadding(
+      child: TextFormField(
+        initialValue: cubit.state.name.value,
+        onChanged: (value) => cubit.updateName(value),
+        decoration: const InputDecoration(
+          isDense: true,
+          fillColor: Colors.white,
+          filled: true,
+          labelText: 'Name',
+        ),
       ),
     );
   }
