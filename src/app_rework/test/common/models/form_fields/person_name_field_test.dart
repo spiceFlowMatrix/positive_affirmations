8a75f50836e3548863bb72fd12b399e4005f68a8
HRahimy <rahimy.hamza@gmail.com>
2022-04-19 18:27:05 +0300

    Mobile app
    Add unit tests for `PersonNameField` model

---
+++ src/app_rework/test/common/models/form_fields/person_name_field_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:positive_affirmations/common/models/form_fields/person_name_field.dart';
+
+void main() {
+  const validNameString = 'Valid Name';
+  const invalidSpecialNameString = '1.2@3#.\$5%6^7&8*9()\\/';
+  const invalidNumericNameString = 'Valid1 Name2';
+
+  group('[PersonNameField]', () {
+    group('[Constructors]', () {
+      test('pure creates valid instance', () {
+        expect(
+          const PersonNameField.pure().pure,
+          isTrue,
+        );
+      });
+      test('dirty creates valid instance', () {
+        expect(
+          const PersonNameField.dirty(validNameString).pure,
+          isFalse,
+        );
+      });
+    });
+
+    group('[Validators]', () {
+      test('error is null given valid value', () {
+        expect(
+          const PersonNameField.dirty(validNameString).error,
+          isNull,
+        );
+      });
+      test('returns `invalid` error given value has special characters', () {
+        expect(
+          const PersonNameField.dirty(invalidSpecialNameString).error,
+          PersonNameFieldValidationError.invalid,
+        );
+      });
+      test('returns `invalid` error given value contains numbers', () {
+        expect(
+          const PersonNameField.dirty(invalidNumericNameString).error,
+          PersonNameFieldValidationError.invalid,
+        );
+      });
+      test('returns `empty` error given value is an empty string', () {
+        expect(
+          const PersonNameField.dirty('').error,
+          PersonNameFieldValidationError.empty,
+        );
+      });
+    });
+  });
+}
