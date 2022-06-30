e646b4096bf975f11f73b3f5cc75149790e7613a
HRahimy <rahimy.hamza@gmail.com>
2022-06-30 13:13:53 +0300

    [Web stack] Get affirmation list query
    Add complete suite of mainline tests

--- src/web-stack/libs/application/src/lib/query/handlers/get-affirmation-list.handler.spec.ts
+++ src/web-stack/libs/application/src/lib/query/handlers/get-affirmation-list.handler.spec.ts
@@ -1,16 +1,18 @@
 import { Test } from '@nestjs/testing';
 import { GetAffirmationListHandler } from './get-affirmation-list.handler';
 import {
+  AffirmationDto,
   AffirmationEntity,
+  AffirmationObjectResponseDto,
   AffirmationRepository,
   affirmationStub,
   FirebaseUserInfo,
+  GetAffirmationListQueryResponseDto,
   MissingRequiredParamException,
   userStub,
 } from '@web-stack/domain';
 import { AuthUserService } from '../../services/auth-user.service';
 import { GetAffirmationListQuery } from '../impl/get-affirmation-list.query';
-import { ResourceLimits } from 'worker_threads';
 
 describe('GetAffirmationListHandler', () => {
   const _userStub = userStub();
@@ -40,15 +42,19 @@ describe('GetAffirmationListHandler', () => {
     authUser,
   });
 
+  const _likedAffirmations = [_affirmationStub];
+
   const mockCreateQueryBuilder = {
     where: jest.fn().mockReturnThis(),
     setParameter: jest.fn().mockReturnThis(),
     leftJoinAndSelect: jest.fn().mockReturnThis(),
     getOne: jest.fn().mockReturnThis(),
-    getMany: jest.fn().mockResolvedValue([_affirmationStub]),
+    getMany: jest.fn().mockResolvedValue(_likedAffirmations),
     andWhere: jest.fn().mockReturnThis(),
     orderBy: jest.fn().mockReturnThis(),
   };
+
+  const [_findResults, _findTotal] = [[_affirmationStub], 1];
 
   beforeEach(async () => {
     mockAffirmationRepository = {
@@ -56,8 +62,8 @@ describe('GetAffirmationListHandler', () => {
       findAndCount: jest
         .fn()
         .mockResolvedValue(<[AffirmationEntity[], number]>[
-          [_affirmationStub],
-          1,
+          _findResults,
+          _findTotal,
         ]),
       save: jest.fn().mockResolvedValue(_affirmationStub),
       getLikedAffirmationsIn: jest.fn().mockResolvedValue([_affirmationStub]),
@@ -130,5 +136,101 @@ describe('GetAffirmationListHandler', () => {
     jest.spyOn(userService, 'user');
     await handler.execute(validQuery);
     expect(userService.user).toHaveBeenCalledWith(authUser);
+  });
+
+  describe('fetching paginated list of affirmations', () => {
+    it('fetches affirmations from repository based on given args', async () => {
+      jest.spyOn(affirmationRepository, 'findAndCount');
+      await handler.execute(validQuery);
+      expect(affirmationRepository.findAndCount).toHaveBeenCalledWith({
+        relations: ['createdBy'],
+        skip: validQuery.skip,
+        take: validQuery.take,
+      });
+    });
+
+    describe('no affirmations exist', () => {
+      let resultDto: GetAffirmationListQueryResponseDto;
+      beforeEach(async () => {
+        jest
+          .spyOn(affirmationRepository, 'findAndCount')
+          .mockResolvedValue([[], 0]);
+        resultDto = await handler.execute(validQuery);
+      });
+
+      it('does not build query to filter likes', () => {
+        jest.spyOn(affirmationRepository, 'createQueryBuilder');
+        expect(affirmationRepository.createQueryBuilder).toBeCalledTimes(0);
+      });
+
+      it('returns valid dto with empty list of affirmations', () => {
+        const expectedDto = new GetAffirmationListQueryResponseDto({
+          totalResults: 0,
+          results: [],
+        });
+
+        expect(resultDto).toEqual(expectedDto);
+      });
+    });
+
+    describe('returns list of affirmations', () => {
+      let resultDto: GetAffirmationListQueryResponseDto;
+      beforeEach(async () => {
+        resultDto = await handler.execute(validQuery);
+      });
+
+      it('returns valid dto with list of affirmations showing likes', () => {
+        const expectedDto = new GetAffirmationListQueryResponseDto({
+          totalResults: _findTotal,
+          results: _findResults.map((result) => {
+            return new AffirmationObjectResponseDto({
+              affirmationData: new AffirmationDto({ ...result }),
+              liked:
+                _likedAffirmations.filter((e) => e.id == result.id).length > 0,
+            });
+          }),
+        });
+
+        expect(resultDto).toEqual(expectedDto);
+      });
+
+      it('builds query to fetch likes for returned affirmations', async () => {
+        // Solution for testing chained functions adapted from:
+        // https://stackoverflow.com/a/57727703/5472560
+        jest.spyOn(affirmationRepository, 'createQueryBuilder');
+
+        expect(affirmationRepository.createQueryBuilder).toHaveBeenCalledWith(
+          'affirmation'
+        );
+
+        expect(
+          affirmationRepository.createQueryBuilder().leftJoinAndSelect
+        ).toHaveBeenCalledWith('affirmation.likes', 'like');
+
+        expect(
+          affirmationRepository.createQueryBuilder().leftJoinAndSelect
+        ).toHaveBeenCalledWith('like.byUser', 'likedBy');
+
+        expect(
+          affirmationRepository.createQueryBuilder().where
+        ).toHaveBeenCalledWith('affirmation.id IN (:...affirmationIds)', {
+          affirmationIds: _findResults.map((e) => e.id),
+        });
+
+        expect(
+          affirmationRepository.createQueryBuilder().andWhere
+        ).toHaveBeenCalledWith('likedBy.dbId = :byUserId', {
+          byUserId: _userStub.dbId,
+        });
+
+        expect(
+          affirmationRepository.createQueryBuilder().orderBy
+        ).toHaveBeenCalledWith('affirmation.createdOn', 'DESC');
+
+        expect(
+          affirmationRepository.createQueryBuilder().getMany
+        ).toHaveBeenCalled();
+      });
+    });
   });
 });
